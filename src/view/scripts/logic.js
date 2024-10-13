a49f8d38e7074cc382f884c3afcede312da29e93
danielscz <danielscz.dsa@gmail.com>
2024-10-12 20:11:50 -0500

    Fix: fetch al backend ya funciona

--- src/view/scripts/logic.js
+++ src/view/scripts/logic.js
@@ -3,12 +3,12 @@ const formElement = document.getElementById('form-agenda')
 formElement.addEventListener('submit', (event) => {
   event.preventDefault()
   const agenda = document.getElementById('POST-agenda').value
-  const agendaJSON = JSON.stringify(agenda)
+  const agendaJson = JSON.stringify({ agenda })
   fetch('http://localhost:4000/api/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: agendaJSON
+    body: agendaJson
   })
 })
